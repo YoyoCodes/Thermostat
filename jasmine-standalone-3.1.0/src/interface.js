02273e325d2ef5a81affc9163ef3305697dcb3cb
Laura Petre <laura.d.petre@gmail.com>
2018-06-15 22:07:52 +0100

    Implemented weather report trough API

--- jasmine-standalone-3.1.0/src/interface.js
+++ jasmine-standalone-3.1.0/src/interface.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
 
-  function updateTemperature() {
-    $('#temperature').text(thermostat.temperature);
-    $('#temperature').attr('class', thermostat.powerUsage());
-  };
+  // $.get("http://localhost:9292", function(data) {
+  //   $('#temperature').text(data.temperature);
+  // });
 
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed'
+    var units = '&units=metric'
+
+    $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
+  });
   var thermostat = new Thermostat();
-  updateTemperature();
+  //
+  // var API_data = $.getJSON('http://localhost:9292');
+  //
+  // var saved_current_city = API_data.responseJSON.current_city;
+  // var saved_temperature = API_data.responseJSON.temperature;
+
+
+  $('#temperature').text(thermostat.temperature);
 
   $('#temperature-up').click(function() {
     thermostat.up();
